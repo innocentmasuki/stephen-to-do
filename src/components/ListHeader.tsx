65f41efbabf15efd8493142242350453515e0d38
innocentmasuki <innocentmasuki99@gmail.com>
2025-11-30 00:16:40 +0300

    update ListHeader component to adjust margin for improved layout

--- src/components/ListHeader.tsx
+++ src/components/ListHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ListHeader = () => {
     return (
-        <div className={'w-full flex flex-col items-start py-5 px-5'}>
+        <div className={'w-full mt-10 flex flex-col items-start py-5 px-5'}>
             <div className={'w-full flex flex-row justify-between '}>
                 <div className={'flex flex-row items-center justify-center size-10 bg-orange-300 rounded-full'}>💼</div>
                 {/*<div>right side</div>*/}
